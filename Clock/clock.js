b51cacfbec131770fc1f27f22ce8b0116abb31c7
About-5 <37988112+About-5@users.noreply.github.com>
2019-11-22 00:39:43 -0800

    Add files via upload

---
+++ Clock/clock.js
@@ -0,0 +1,82 @@
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
+let fontSize = 40;
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+addEventListener("resize", function() {
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
+    init();
+});
+
+function randomIntFromRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function Clock(x, y) {
+    this.x = x;
+    this.y = y;
+    this.velocity = {
+        x: 1,
+        y: 1
+    }
+    this.color = `rgb(${randomIntFromRange(0, 255)}, ${randomIntFromRange(0, 255)}, ${randomIntFromRange(0, 255)})`;
+}
+
+Clock.prototype.draw = function() {
+    ctx.font = `${fontSize}px Arial`;
+    ctx.fillStyle = this.color;
+    ctx.fillText(this.myTime, this.x, this.y);
+    ctx.shadowColor = '#E3EAEF';
+    ctx.shadowBlur = 30;
+    ctx.stroke();
+}
+
+Clock.prototype.update = function() {
+    if(this.x + this.velocity.x + 270 > canvas.width || this.x + this.velocity.x < 0) {
+        this.velocity.x = -this.velocity.x;
+        this.color = `rgb(${randomIntFromRange(0, 255)}, ${randomIntFromRange(0, 255)}, ${randomIntFromRange(0, 255)})`
+    }
+    if(this.y + this.velocity.y > canvas.height || this.y + this.velocity.y - 30 < 0) {
+        this.velocity.y = -this.velocity.y;
+        this.color = `rgb(${randomIntFromRange(0, 255)}, ${randomIntFromRange(0, 255)}, ${randomIntFromRange(0, 255)})`
+    }
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+    console.log(this.velocity);
+    this.date = new Date();
+    this.hour = this.date.getHours();
+    this.min = this.date.getMinutes();
+    this.sec = this.date.getSeconds();
+    this.midday = "AM";
+    this.midday = (this.hour >= 12) ? "PM" : "AM";
+    this.hour = (this.hour ==0) ? 12: ((this.hour > 12) ? (this.hour - 12): this.hour);
+    this.hour = (this.hour < 10) ? "0" + this.hour : this.hour;
+    this.min = (this.min < 10) ? "0" + this.min : this.min;
+    this.sec = (this.sec < 10) ? "0" + this.sec : this.sec;
+    this.myTime = this.hour + " : " + this.min + " : " + this.sec + " " + this.midday;
+    this.draw();
+}
+
+function move() {
+
+}
+
+let currentTime;
+
+function init() {
+    currentTime = new Clock(canvas.width / 2 - fontSize / 2 - 135, canvas.height / 2 - fontSize / 2);
+}
+function animate() {
+    requestAnimationFrame(animate);
+    
+    ctx.fillStyle = '#113542'
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    currentTime.update();
+    
+}
+
+init();
+animate();
